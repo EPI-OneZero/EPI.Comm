96af27894ce8296fee6f3485616d2c72c2229c85
Minki Ha <bigear9304@gmail.com>
2023-10-01 11:33:24 +0900

    1

---
+++ UnitTest/Etc/EtcTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnitTest.Etc
+{
+    [TestClass]
+    public class EtcTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var list = new List<int>
+            {
+            };
+            var reads = list.AsReadOnly();
+            var th1 = new Thread(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    list.Remove(i);
+                    list.Remove(i);
+                    list.Remove(i);
+                    list.Remove(i);
+                    list.Remove(i);
+                    list.Add(i);
+                    list.Add(i);
+                    list.Add(i);
+                    list.Add(i);
+                    list.Add(i);
+                    Thread.Sleep(1);
+                    Console.WriteLine("1 : " +list.Count);
+                }
+            });
+            var th2 = new Thread(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    var x= list.ToArray();
+                    var y= list.ToList();
+                    Console.WriteLine("2 : "+ x.Length);
+                    Thread.Sleep(1);
+                }
+            });
+            th2.Start();
+            th1.Start();
+        }
+    }
+}
